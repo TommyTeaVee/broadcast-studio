4cbacfce23bfa70ae8c6d334c6a373971d5451e0
ZDark <zdark@ZDarks-MacBook-Pro.local>
2021-09-28 20:54:02 -0400

    corrected the macro api call which now utilizes a shortcut route.  Started to get the video preview working but will need to fix a buffer issue.

--- src/js/components/main/PreviewWindow.js
+++ src/js/components/main/PreviewWindow.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 import {getImage} from '../../../../api/NewTekTricasterApi'
 
+const WebSocket = require('ws')
+const DatauriParser = require('datauri/parser')
+
 class PreviewWindowButton extends React.Component {
     render() {
         return (
@@ -21,10 +24,25 @@ class PreviewWindow extends React.Component {
             this.setState({imageURL: URL.createObjectURL(image)})
         })
     }
+    startPreviewStream = () => {
+        var url = `ws://${this.props.tricasterAddress}/v1/video_notifications?name=output1&xres=640&yres=480&q=100`
+        var ws = new WebSocket(url)
+        ws.onmessage = (message) => {
+            new Response(message.data)
+                .arrayBuffer()
+                .then(buffer => {
+                    const parser = new DatauriParser()
+                    let image = parser.format('jpeg',buffer)
+                    console.log('image = ',image)
+                    this.setState({imageURL: image.content})
+
+                })
+        }
+    }
     render() {
         return (
             <div id='preview-window'>
-                <button onClick={this.getPreviewImage}>Get Preview Image</button>
+                <button onClick={()=> {this.startPreviewStream()}}>Get Preview Image</button>
                 <img src={this.state.imageURL}/>
             </div>  
         )
